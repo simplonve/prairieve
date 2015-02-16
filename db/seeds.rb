5ac5d7fdc76427375008a4302ee25e4708073101
Vincent Jumeaux <vincent.jumeaux@gmail.com>
2015-02-16 21:32:35 +0200

    ressouce seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Ressource.create({ title: 'Declaration du Cyberespace',
+                   url: 'http://editions-hache.com/essais/barlow/barlow2.html',
+                   lang: 'en',
+                   level: 'green',
+                   time: 20,
+                   created_at: '2015-02-15 21:27:20'})
+
+Ressource.create({ title: "Qu'est-ce qu'une fonction ?",
+                   url: 'http://www-math.mit.edu/~djk/calculus_beginners/chapter03/contents.html',
+                   lang: 'en',
+                   level: 'blue',
+                   time: 20,
+                   created_at: '2015-02-16 21:27:20'})
