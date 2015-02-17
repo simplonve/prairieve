03c49630f6996d464b165a1d5fc7df6fdf09c070
Vincent Jumeaux <vincent.jumeaux@gmail.com>
2015-02-17 13:06:58 +0200

    more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,4 @@
-Chapitre.create({ id: 1,
-                  title: 'Hello World',
+Chapitre.create({ title: 'Hello World',
                   intro: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
                   })
 Ressource.create({ title: 'Declaration du Cyberespace',
@@ -17,3 +16,21 @@ Ressource.create({ title: "Qu'est-ce qu'une fonction ?",
                    time: 20,
                    created_at: '2015-02-16 21:27:20',
                    chapitre_id: 1})
+Chapitre.create({ title: 'Next step',
+                  intro: 'Lorem ipsum dolor sit ame, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+                  })
+Ressource.create({ title: "Qu'est-ce que l'internet",
+                   url: 'http://editions-hache.com/essais/barlow/barlow2.html',
+                   lang: 'en',
+                   level: 'green',
+                   time: 20,
+                   created_at: '2015-02-15 21:27:20',
+                   chapitre_id: 2})
+
+Ressource.create({ title: "Qu'est-ce qu'un programme ?",
+                   url: 'http://www-math.mit.edu/~djk/calculus_beginners/chapter03/contents.html',
+                   lang: 'en',
+                   level: 'blue',
+                   time: 20,
+                   created_at: '2015-02-16 21:27:20',
+                   chapitre_id: 2})
