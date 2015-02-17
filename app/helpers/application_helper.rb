a80ae9b3a06f4814b2a1b074c9ac682682fd3e9a
Vincent Jumeaux <vincent.jumeaux@gmail.com>
2015-02-17 17:49:12 +0200

    Frontend viz + partial and helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def display_lang(obj)
+    case obj.lang
+    when 'en' then "#{image_tag 'uk.png', size: '20x20'}"
+    when 'fr' then "#{image_tag 'fr.png', size: '20x20'}"
+    end
+  end
+
+  def display_level(obj)
+    case obj.level
+    when 'verte' then "<div class='label piste_verte'> verte </div>"
+    when 'bleue' then "<div class='label piste_bleue'> bleue </div>"
+    when 'rouge' then "<div class='label piste_rouge'> rouge </div>"
+    when 'noire' then "<div class='label piste_noire'> noire </div>"
+    end
+  end
 end
