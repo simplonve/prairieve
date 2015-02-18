2d815effc3437ec9d471c11996fbec5461da0450
Vincent Jumeaux <vincent.jumeaux@gmail.com>
2015-02-18 10:40:50 +0200

    scope to order chapitre categories

--- app/models/ressource.rb
+++ app/models/ressource.rb
@@ -1,4 +1,8 @@
 class Ressource < ActiveRecord::Base
   validates_uniqueness_of :title
   belongs_to :chapitre
+
+  scope :lecture, ->  { where(categorie: "lecture") }
+  scope :exercice, ->  { where(categorie: "exercice") }
+  scope :bonus, ->  { where(categorie: "bonus") }
 end
