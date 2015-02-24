b69c46f1db15091f61a97ec3d25956c008c82ea8
Vincent Jumeaux <vincent.jumeaux@gmail.com>
2015-02-24 17:34:51 +0200

    views for user with css & locale

--- app/controllers/chapitres_controller.rb
+++ app/controllers/chapitres_controller.rb
@@ -1,7 +1,6 @@
 class ChapitresController < ApplicationController
   skip_filter :authenticate_user, only: [:welcome]
 
-
   def index
     @chapitres = Chapitre.all
   end
@@ -11,13 +10,5 @@ class ChapitresController < ApplicationController
   end
 
   def welcome
-  end
-
-  protected
-
-  def authenticate
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV['LOGUSR'] && password == ENV['LOGPWD']  
-    end
   end
 end
