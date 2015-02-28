95dfc9c9c0026383dd1e8a6587e47afe2d7a72db
Vincent Jumeaux <vincent.jumeaux@gmail.com>
2015-02-28 17:46:45 +0200

    view form feedback form and display with bad jquery

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,13 @@
 module ApplicationHelper
-  def display_lang(obj)
-    case obj.lang
+  def display_lang(lang)
+    case lang
     when 'en' then "#{image_tag 'uk.png', size: '20x20'}"
     when 'fr' then "#{image_tag 'fr.png', size: '20x20'}"
     end
   end
 
-  def display_level(obj)
-    case obj.level
+  def display_level(level)
+    case level
     when 'verte' then "<div class='label piste_verte'> verte </div>"
     when 'bleue' then "<div class='label piste_bleue'> bleue </div>"
     when 'rouge' then "<div class='label piste_rouge'> rouge </div>"
