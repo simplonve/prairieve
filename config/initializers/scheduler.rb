e6c30c6674cb888f7895495086d44f9568213cb2
Vincent Jumeaux <vincent.jumeaux@gmail.com>
2015-03-09 18:43:05 +0200

    correct date scheduler

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -4,23 +4,18 @@ scheduler = Rufus::Scheduler.new
 
 ary_chapitres = parse_json('db/chapitres.json')
 
-scheduler.at '2015-03-15 09:00:00 +0100' do      
+scheduler.at '2015-03-16 05:00:00' do      
   Chapitre.create_and_mail(ary_chapitres[1])
 end
 
-scheduler.at '2015-03-22 09:00:00 +0100' do
+scheduler.at '2015-03-23 05:00:00' do
   Chapitre.create_and_mail(ary_chapitres[2])
 end
 
-scheduler.at '2015-03-29 09:00:00 +0100' do
+scheduler.at '2015-03-30 05:00:00' do
   Chapitre.create_and_mail(ary_chapitres[3])
 end
 
-scheduler.at '2015-04-05 09:00:00 +0100' do      
+scheduler.at '2015-04-06 05:00:00' do      
   Chapitre.create_and_mail(ary_chapitres[4])
-end
-
-scheduler.at '2015-03-05 15:26:00 +0200' do
-  puts "###############"
-  puts "test scheduler on heroku production"
 end
