070734bbdeaf9596849465f1d9154c68634c138b
Vincent Jumeaux <vincent.jumeaux@gmail.com>
2015-03-23 23:21:09 +0200

    chapitre 3

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -12,10 +12,6 @@ scheduler.at '2015-03-27 18:00:00' do
   Chapitre.create_and_mail(ary_chapitres[2])
 end
 
-scheduler.at '2015-03-30 05:00:00' do
-  Chapitre.create_and_mail(ary_chapitres[3])
-end
-
 scheduler.at '2015-04-06 05:00:00' do      
   Chapitre.create_and_mail(ary_chapitres[4])
 end
