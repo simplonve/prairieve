d9986b178bd53a485169459cbe95877294cf0c8e
Vincent Jumeaux <vincent.jumeaux@gmail.com>
2015-04-12 02:31:53 +0200

    little refactoring

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,6 +41,12 @@ class User < ActiveRecord::Base
     save_and_reset_password(email)
   end
 
+  def assign(activite)
+    unless self.activites.include?(activite)  
+      self.activites << activite
+    end
+  end
+
   private
   def self.generate_password
     chars = [*('a'..'z'),*('A'..'Z'), *('0'..'9')]
