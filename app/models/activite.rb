1884b6f5b1eda98159234d6232c2ddb8af12e09b
Vincent Jumeaux <vincent.jumeaux@gmail.com>
2015-10-18 19:55:11 +0200

    counter of right questions

--- app/models/activite.rb
+++ app/models/activite.rb
@@ -5,4 +5,13 @@ class Activite < ActiveRecord::Base
   has_many :users, through: :valides 
 
   default_scope -> { order('id ASC') }
+
+  def total_invalide(user)
+    question_valides = self.questions.group_by{|qu|  qu.already_valid(user)}[true]
+    if question_valides
+      self.questions.count - question_valides.count 
+    else
+      0
+    end
+  end
 end
